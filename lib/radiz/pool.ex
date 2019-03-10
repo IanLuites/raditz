28147edf84aa2f883d3d76238ee0b8224776def4
Ian Luites <IanLuites@users.noreply.github.com>
2019-03-10 20:22:49 +0900

    Add pool behavior.

---
+++ lib/radiz/pool.ex
@@ -0,0 +1,13 @@
+defmodule Raditz.Pool do
+  @moduledoc ~S"""
+  Connection pool behavior.
+  """
+
+  @callback child_spec(module, Keyword.t()) :: tuple
+
+  @callback command(module, [binary], Keyword.t()) ::
+              {:ok, [Redix.Protocol.redis_value()]} | {:error, atom | Redix.Error.t()}
+
+  @callback multi(module, [[binary]], Keyword.t()) ::
+              {:ok, [Redix.Protocol.redis_value()]} | {:error, atom | Redix.Error.t()}
+end
