47aa494dc268ad0e3a614b0772d71b74666dbffe
Ian Luites <IanLuites@users.noreply.github.com>
2020-07-12 21:56:06 +0200

    Improve pool spec.

--- lib/raditz/pool.ex
+++ lib/raditz/pool.ex
@@ -3,7 +3,7 @@ defmodule Raditz.Pool do
   Connection pool behavior.
   """
 
-  @callback child_spec(module, Keyword.t()) :: tuple
+  @callback child_spec(module, Keyword.t()) :: Supervisor.child_spec() | :supervisor.child_spec()
 
   @callback command(module, [binary], Keyword.t()) ::
               {:ok, [Redix.Protocol.redis_value()]} | {:error, atom | Redix.Error.t()}
